5107fcde8921902a8096d2208edc96fc7ed3e470
Rus <de.mr44@gmail.com>
2022-01-01 17:06:47 +0500

    Implemented the factory

--- Patterns-FactoryMethod/ViewController.swift
+++ Patterns-FactoryMethod/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var exercises = [Exercise]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        addExercise(.jumpingJacks)
+        addExercise(.pullUps)
+        startExercises()
     }
-
-
+    
+    private func addExercise(_ type: ExereciseTypes) {
+        let exercise = ExerciseFactory.defaultFactory.create(type: type)
+        exercises.append(exercise)
+    }
+    
+    private func startExercises() {
+        for exercise in exercises {
+            exercise.start()
+            exercise.stop()
+        }
+    }
 }
 
